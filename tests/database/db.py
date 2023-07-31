e9b5cd6d55a5119f276786aee392fdc774d5a72b
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-07-31 17:19:47 -0300

    create config file and read database url from it

--- tests/database/db.py
+++ tests/database/db.py
@@ -2,12 +2,11 @@ import os
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+import json
 
-
-load_dotenv()
-
-TEST_DATABASE_URL = os.environ.get("TEST_DATABASE_URL")
-assert TEST_DATABASE_URL is not None
+with open("./config_test.json") as f:
+    TEST_DATABASE_URL = json.load(f)["test_database"]
+    print(TEST_DATABASE_URL)
 
 engine_test = create_engine(TEST_DATABASE_URL)
 sessionmaker_test = sessionmaker(bind=engine_test)
