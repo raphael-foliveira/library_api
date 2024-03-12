3710a66e815a4b4d54df5beac300b74ff3215461
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-12 19:13:29 -0300

    tests: use setup_function instead of pytest.fixture to setup tests

--- tests/authors/test_authors.py
+++ tests/authors/test_authors.py
@@ -13,8 +13,7 @@ client = TestClient(app)
 author_repository_mock: AuthorRepository = mock.Mock()
 
 
-@pytest.fixture
-def setup():
+def setup_function():
     author_repository_mock.list.return_value = authors_entities_stub
     author_repository_mock.find.return_value = authors_entities_stub[0]
     author_repository_mock.create.return_value = authors_entities_stub[0]
@@ -29,41 +28,42 @@ def setup_module():
     app.dependency_overrides[get_author_repository] = override_get_author_repository
 
 
-def test_get_all_authors(setup):
+def test_get_all_authors():
     response = client.get("/authors")
+    body = response.json()
     assert response.status_code == 200
-    assert isinstance(response.json(), list)
-    assert len(response.json()) == len(authors_entities_stub)
+    assert isinstance(body, list)
+    assert len(body) == len(authors_entities_stub)
 
 
-def test_find_author(setup):
+def test_find_author():
     response = client.get(f"/authors/")
     assert response.status_code == 200
 
 
-def test_find_non_existing_author(setup):
+def test_find_non_existing_author():
     non_existing_author_id = 650
     author_repository_mock.find.return_value = None
     response = client.get(f"/authors/{non_existing_author_id}")
     assert response.status_code == 404
 
 
-def test_create_author(setup):
+def test_create_author():
     response = client.post("/authors", json={"first_name": "John", "last_name": "Doe"})
     assert response.status_code == 201
 
 
-def test_create_invalid_author(setup):
+def test_create_invalid_author():
     response = client.post("/authors", json={"foo": "John", "bar": "Doe"})
     assert response.status_code == 422
 
 
-def test_delete_author(setup):
+def test_delete_author():
     response = client.delete(f"/authors/{authors_entities_stub[0].id}")
     assert response.status_code == 204
 
 
-def test_delete_non_existing_author(setup):
+def test_delete_non_existing_author():
     non_existing_author_id = 650
     author_repository_mock.delete.return_value = False
     response = client.delete(f"/authors/{non_existing_author_id}")
--- tests/books/test_books.py
+++ tests/books/test_books.py
@@ -23,8 +23,7 @@ def override_get_books_repository():
     return book_repository_mock
 
 
-@pytest.fixture
-def setup():
+def setup_function():
     book_repository_mock.list.return_value = books_stub
     book_repository_mock.find.return_value = books_stub[0]
     book_repository_mock.create.return_value = books_stub[0]
@@ -33,26 +32,26 @@ def setup():
     app.dependency_overrides[get_books_repository] = override_get_books_repository
 
 
-def test_get_all_books(setup):
+def test_get_all_books():
     response = client.get("/books/")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert len(response.json()) == len(books_stub)
 
 
-def test_find_book(setup):
+def test_find_book():
     response = client.get(f"/books/{books_stub[0].id}")
     assert response.status_code == 200
 
 
-def test_find_non_existing_book(setup):
+def test_find_non_existing_book():
     non_existing_book_id = 650
     book_repository_mock.find.return_value = None
     response = client.get(f"/books/{non_existing_book_id}")
     assert response.status_code == 404
 
 
-def test_create_book(setup):
+def test_create_book():
     mock_book = fake_book_schema()
     mock_book.author_id = 1
     form_data: Mapping[str, Any] = {
@@ -70,7 +69,7 @@ def test_create_book(setup):
     assert response.status_code == 201
 
 
-def test_create_invalid_book(setup):
+def test_create_invalid_book():
     form_data: Mapping[str, Any] = {"foo": "bar", "spam": "eggs"}
     response = client.post(
         "/books/",
@@ -79,12 +78,12 @@ def test_create_invalid_book(setup):
     assert response.status_code == 422
 
 
-def test_delete_book(setup):
+def test_delete_book():
     response = client.delete(f"/books/{books_stub[0].id}")
     assert response.status_code == 204
 
 
-def test_delete_non_existing_book(setup):
+def test_delete_non_existing_book():
     non_existing_book_id = 650
     book_repository_mock.delete.return_value = False
     response = client.delete(f"/books/{non_existing_book_id}")
@@ -92,6 +91,6 @@ def test_delete_non_existing_book(setup):
     book_repository_mock.delete.return_value = True
 
 
-def test_get_upload_path(setup):
+def test_get_upload_path():
     upload_path = get_upload_path("1")
     assert upload_path == "./uploads/1"
