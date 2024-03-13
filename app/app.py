fdf02944caa5a262125123893fd78965d5fa2b53
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-12 21:01:00 -0300

    refactor: call metadata.create_all before instantiating App

--- app/app.py
+++ app/app.py
@@ -6,8 +6,8 @@ from app.modules.authors.routes import authors_router
 from app.modules.books.routes import books_router
 
 
-app = FastAPI(responses={404: {"error": "not found"}})
 Base.metadata.create_all(bind=engine)
+app = FastAPI(responses={404: {"error": "not found"}})
 
 app.add_middleware(
     CORSMiddleware,
