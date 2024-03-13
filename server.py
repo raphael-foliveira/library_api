6a1dcc4570a93ee1e3742775d7878315caf006f1
Raphael Oliveira <raphael.foliveira1@gmail.com>
2024-03-13 12:03:59 -0300

    refactor: move mocks to their own separate files

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import uvicorn
 
+from app.database.config import Base, engine
+
 if __name__ == "__main__":
+    Base.metadata.create_all(bind=engine)
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
--- tests/authors/test_authors.py
+++ tests/authors/test_authors.py
@@ -4,26 +4,17 @@ from app.app import app
 from app.modules.authors.repository import AuthorRepository
 from app.modules.authors.routes import get_author_repository
 from ..stubs.author_stubs import authors_entities_stub
+from ..mocks import author_mocks
 
 client = TestClient(app)
 
 
-author_repository_mock: AuthorRepository = mock.Mock()
-
-
-def initialize_mocks():
-    author_repository_mock.list.return_value = authors_entities_stub
-    author_repository_mock.find.return_value = authors_entities_stub[0]
-    author_repository_mock.create.return_value = authors_entities_stub[0]
-    author_repository_mock.delete.return_value = True
-
-
 def setup_function():
-    initialize_mocks()
+    author_mocks.initialize()
 
 
 def override_get_author_repository():
-    return author_repository_mock
+    return author_mocks.repository
 
 
 def setup_module():
@@ -45,7 +36,7 @@ def test_find_author():
 
 def test_find_non_existing_author():
     non_existing_author_id = 650
-    author_repository_mock.find.return_value = None
+    author_mocks.repository.find.return_value = None
     response = client.get(f"/authors/{non_existing_author_id}")
     assert response.status_code == 404
 
@@ -67,7 +58,7 @@ def test_delete_author():
 
 def test_delete_non_existing_author():
     non_existing_author_id = 650
-    author_repository_mock.delete.return_value = False
+    author_mocks.repository.delete.return_value = False
     response = client.delete(f"/authors/{non_existing_author_id}")
-    author_repository_mock.delete.return_value = True
+    author_mocks.repository.delete.return_value = True
     assert response.status_code == 404
